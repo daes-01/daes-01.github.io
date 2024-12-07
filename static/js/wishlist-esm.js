fba2bd7057c49be774ea5b98f3edf3839997aada
Scott Yamamoto <scott.yamamoto@hotmail.com>
2024-12-07 15:01:25 -0500

    initial commit

---
+++ static/js/wishlist-esm.js
@@ -0,0 +1,42 @@
+const wishlists = [
+  {
+    username: "Yuni",
+    list : [
+      "mammon",
+      "Kanae",
+      "Idia",
+      "Ensemble Stars ",
+      "Twisted wonderland ",
+      "Hypnosis mic",
+      "Blackstar theater",
+      "Sukuna",
+      "Yuji",
+      "Nobara",
+      "Mahito",
+      "Nanami",
+      "Maki zenin",
+      "Helios rising heroes",
+      "Nu carnival",
+      "Idolish7 ",
+      "B-project ",
+      "Tears of themis",
+      "Male utaite",
+      "Holostars",
+      "For all time",
+      "Uta prince",
+      "18 trip",
+      "Inosuke",
+      "Snake hashira ",
+      "Mist hashira",
+      "Top 3 upper demons",
+      "Muzan",
+      "Sk8",
+      "Diabolik lovers",
+      "Promise of wizard",
+      "Todoroki",
+      "Bakugo",
+    ]
+  },
+];
+
+export { wishlists };
